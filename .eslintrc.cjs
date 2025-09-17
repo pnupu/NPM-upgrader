613188b2d8a7cbc2420d6a923cd5382cc797cfd4
Ilkka Lappeteläinen <ilkka.lappetelainen@gmail.com>
2025-09-18 00:22:05 +0300

    chore(repo): add monorepo configs (pnpm, tsconfig, eslint)

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+    browser: false,
+    jest: false,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
+  ],
+  ignorePatterns: [
+    'dist/**',
+    'node_modules/**',
+    '**/*.d.ts',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
