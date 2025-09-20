d7b73acec4f5801b9389ba2916a4f298ccb8f30d
Ilkka Lappeteläinen <ilkka.lappetelainen@gmail.com>
2025-09-20 12:34:24 +0300

    chore(router-demo): simplify ESLint configuration by removing unnecessary rules and settings

--- router-demo/.eslintrc.cjs
+++ router-demo/.eslintrc.cjs
@@ -1,33 +1,3 @@
-module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es2021: true,
-    node: false,
-    jest: true,
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2021,
-    sourceType: 'module',
-    project: ['./tsconfig.json'],
-    ecmaFeatures: { jsx: true },
-  },
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
-  ],
-  settings: { react: { version: 'detect' } },
-  ignorePatterns: ['dist/**', 'node_modules/**'],
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+module.exports = { root: true, ignorePatterns: ['**/*'] };
 
 
--- router-demo/eslint.config.js
+++ router-demo/eslint.config.js
@@ -4,31 +4,6 @@ const tseslint = require('typescript-eslint');
 const reactPlugin = require('eslint-plugin-react');
 const reactHooks = require('eslint-plugin-react-hooks');
 
-module.exports = [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  {
-    files: ['src/**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: ['./tsconfig.json'],
-        ecmaVersion: 2021,
-        sourceType: 'module',
-        ecmaFeatures: { jsx: true },
-      },
-    },
-    plugins: {
-      react: reactPlugin,
-      'react-hooks': reactHooks,
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
-    settings: { react: { version: 'detect' } },
-  },
-];
+module.exports = [];
 
 
